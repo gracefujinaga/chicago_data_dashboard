dd2caa0af1929257adbbdc6ae3508638dfbaec9c
Grace Fujinaga <gracefujinaga@gmail.com>
2024-12-04 16:26:42 -0600

    cloudbuild options and front end

--- src/flask-app/main.py
+++ src/flask-app/main.py
@@ -1,41 +1,16 @@
 from flask import Flask, render_template
-from datetime import datetime
-
-# from helper_v4 import forecastr,determine_timeframe,get_summary_stats,validate_model,preprocessing
 import logging
-import time
-
 import pandas as pd
-
-import datetime
-from datetime import datetime, date, timedelta
-import time
-from math import isnan
-import numpy as np
-
-import seaborn as sns
-
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
 from prophet import Prophet
-from prophet.diagnostics import cross_validation
-from prophet.diagnostics import performance_metrics
-from prophet.plot import plot_cross_validation_metric
 from prophet.plot import plot_plotly, plot_components_plotly
-
-
 from flask import Flask, jsonify
 from flask import Flask, request, jsonify
 import requests
 import pandas as pd
-
+import plotly.subplots as sp
 import os
-
 import logging
 
 # setup logging
@@ -46,7 +21,6 @@ go_microservice_url = os.getenv("go_microservice_url")
 base_url = go_microservice_url
 print(base_url)
 
-
 app = Flask(__name__)
 
 # Route for the homepage
@@ -55,17 +29,66 @@ def home():
     app.logger.info('Home page accessed')
     return render_template('homepage.html')
 
-@app.route('/test', methods=['GET'])
-def test():
-    app.logger.info('Test page accessed')
+# route for zip codes
+@app.route('/trips_by_zip', methods=['Get'])
+def trips_by_zip():
+    zipcode = request.args.get('zipcode', None)
 
+    if not zipcode:
+        return render_template('no_zip.html', zipcode=zipcode)
+
+     # Fetch data
     response = requests.get(f'{base_url}req4')
     if response.status_code != 200:
-        return jsonify({'error': 'Failed to fetch data from Go service'}), 500
-
+        print('Error fetching data')
+        exit()
+ 
+    # Create a DataFrame from API response
     data = response.json()
-    df = pd.DataFrame(data) 
-    return data
+    df = pd.DataFrame(data)
+
+    if zipcode not in df['pickup_zip_code'].values and zipcode not in df['dropoff_zip_code'].values:
+        return render_template('no_zip.html', zipcode=zipcode)
+
+    df['trip_start_timestamp'] = pd.to_datetime(df['trip_start_timestamp'], utc=True)
+    df['trip_end_timestamp'] = pd.to_datetime(df['trip_end_timestamp'], utc=True)
+    df['date'] = df['trip_start_timestamp'].dt.date
+
+    dropoff_df = df[df['dropoff_zip_code'] == zipcode].copy()
+    pickup_df = df[df['pickup_zip_code'] == zipcode].copy()
+
+    dropoff_df['type'] = 'Dropoff'
+    pickup_df['type'] = 'Pickup'
+
+    # get plotting data
+    dropoff_count_df = dropoff_df.groupby(['date'])['trip_id'].count().reset_index(name='Total_Trips')
+    pickup_count_df = pickup_df.groupby(['date'])['trip_id'].count().reset_index(name='Total_Trips')
+    combined_df = pd.concat([dropoff_df, pickup_df], ignore_index=True)
+    combined_count_df = combined_df.groupby(['date', 'type'])['trip_id'].count().reset_index(name='Total_Trips')
+
+    # Create a subplot with 3 rows
+    fig = sp.make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.1,
+                        subplot_titles=[f'Dropoffs in Zipcode {zipcode}', 
+                                        f'Pickups in Zipcode {zipcode}', 
+                                        f'Dropoffs and Pickups in Zipcode {zipcode}'])
+
+    # add plots to subplot
+    fig.add_trace(go.Scatter(x=dropoff_count_df['date'], y=dropoff_count_df['Total_Trips'], mode='lines+markers', name='Dropoffs', line=dict(color='blue')), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pickup_count_df['date'], y=pickup_count_df['Total_Trips'], mode='lines+markers', name='Pickups', line=dict(color='green')), row=2, col=1)
+    fig.add_trace(go.Scatter(x=combined_count_df['date'], y=combined_count_df['Total_Trips'], mode='lines+markers', name='Combined', line=dict(color='red')), row=3, col=1)
+
+    # Update layout
+    fig.update_layout(title_text=f'Trip Counts for Zipcode {zipcode}', 
+                    showlegend=True, 
+                    height=900)
+
+    # Save the figure as an HTML file
+    figure_html = fig.to_html(full_html=False)
+
+    return render_template('analysis.html', 
+                           page_title=f'Trips By Zipcode: {zipcode}',
+                           graph=figure_html
+                           )
 
 @app.route('/dropoffs', methods=['GET'])
 def dropoffs():
@@ -94,6 +117,8 @@ def create_forecast_page(grouping_col, title, zipcode = None):
 
     df['trip_start_timestamp'] = pd.to_datetime(df['trip_start_timestamp'], utc=True)
     df['trip_end_timestamp'] = pd.to_datetime(df['trip_end_timestamp'], utc=True)
+
+    df['time'] = df['trip_start_timestamp'].dt.tz_localize(None)
 
     df.set_index('trip_start_timestamp', inplace=True)
 
@@ -131,8 +156,8 @@ def create_forecast_page(grouping_col, title, zipcode = None):
     graph_html = fig.to_html(full_html=False)
 
     # Group by date and calculate total trips
-    trip_count_df = df.groupby(['date'])['trip_id'].count().reset_index(name='Total_Trips')
-    plot_df = trip_count_df.rename(columns={'date': 'ds', 'Total_Trips': 'y'})
+    trip_count_df = df.groupby(['time'])['id'].count().reset_index(name='Total_Trips')
+    plot_df = trip_count_df.rename(columns={'time': 'ds', 'Total_Trips': 'y'})
 
     # Train the Prophet model
     model = Prophet(yearly_seasonality=True, daily_seasonality=True)
@@ -150,7 +175,7 @@ def create_forecast_page(grouping_col, title, zipcode = None):
     components_html = components_fig.to_html(full_html=False)
 
     # Return the chart to the template
-    return render_template('forecast.html',
+    return render_template('forecast_page.html',
                            page_title = title,
                            forecast_title = f'Forecast for {title} Zipcodes',
                            barplot_title = f'Bar Plot for {title} Zipcodes',
@@ -161,4 +186,4 @@ def create_forecast_page(grouping_col, title, zipcode = None):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5004)
-    #app.run(host='0.0.0.0', port=5004)
+    #app.run(host='0.0.0.0', port=5000)
