9d2df8ffaf44f3c5ff1e0ccb22449eadafb88635
Grace Fujinaga <gracefujinaga@gmail.com>
2024-12-04 00:35:16 -0600

    fixing port

--- src/flask-app/main.py
+++ src/flask-app/main.py
@@ -143,4 +143,4 @@ def create_forecast_page(grouping_col, title, zipcode = None):
                            forecast_html=forecast_html)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5004)
+    app.run(host='0.0.0.0', port=5000)
